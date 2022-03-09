7108cc61d74da581c68eeedcf6f7b9454900c7ed
root <root@ip-172-31-84-221.ec2.internal>
2022-03-09 10:26:53 +0000

    Task 3 automation part with cron job

--- automation.sh
+++ automation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apache2Installed=$(apache2 -v | grep -i apache)
 s3_bucket=upgrad-abira
-
+name=Abira
 
 sudo apt update -y
 #Check if the apache2 is installed or not
@@ -26,15 +26,50 @@ echo -e '\e[1;36mApache service is enabled\e[1;m'
 
 # .tar all the log files and copy it to /tmp/
 
-filename=Abira-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar.gz
+filename=${name}-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar.gz
 filePath=/tmp/${filename}
 tar -czvf $filePath /var/log/apache2/*.log
 echo -e '\e[1;35m\nThe .tar file is created and copied to /tmp/ directory\e[1;m'
 
-#Check aws cli is installed or not, if yes copy the logs zip file to s3
+#Copy the logs zip file to s3 using aws cli
 
 aws s3 \
 cp $filePath \
 s3://${s3_bucket}/${filename}
 echo -e '\e[1;32m\nSuccessfully .tar file is uploaded into s3 bucket\e[1;m'
 echo $filename is uploaded to s3 bucket: $s3_bucket
+
+#Creating a inventory.html file, if it does not exists
+
+file=/var/www/html/inventory.html
+bold=$(tput bold)
+normal=$(tput sgr0)
+if [ ! -e "$file" ] ; then
+	touch "$file"
+	echo -e '\e[1;32minventory.html file created successfully\e[1;m'
+	echo -e "${bold}Log type \tTime Created \t\tType \tSize${normal}" >> $file
+
+fi
+if [ ! -w "$file" ] ; then
+	echo cannot write to $file
+	exit 1
+fi
+
+# Entry data on the inventory file
+
+logType=httpd-logs
+timeCreated=$(date '+%d%m%Y-%H%M%S')
+fileExtension=${filename%.*}
+fileType=${fileExtension#*.}
+fileSize=$(du -sh $filePath | awk '{print  $1}')
+inputFile="$logType \t$timeCreated \t$fileType \t$fileSize"
+echo -e "$inputFile" >> $file
+
+# check the automation file is present or not on /etc/cron.d, if not create a automation file to add the cron job schedule everyday
+
+cronfile=/etc/cron.d/automation
+if [ ! -e "$cronfile" ] ; then
+	touch "$cronfile"
+	echo "0 12 * * * root /root/Automation_Project/automation.sh" >> $cronfile
+	echo -e '\e[1;35mautomation file on /etc/cron.d/ created successfully\e[1;m'
+fi
